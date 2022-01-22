7391bbb3cb1ac78ed719fde21069cbe61c06140a
S0YKIM <soyeonkim.9683@gmail.com>
2022-01-22 22:55:14 +0900

    단방향과 쌍방향 연결리스트의 노드

--- data_structure.h
+++ data_structure.h
@@ -1,19 +1,20 @@
 #ifndef DATA_STRUCTURE_H
 # define DATA_STRUCTURE_H
+
 # include <stdio.h>
 # include <stdlib.h>
+# include "arrayList.h"
+# include "linkedlist.h"
 
-typedef struct node1 {
+typedef struct s_singleNode {
 	int data;
-	struct node* next;
-} simpleNode;
+	struct t_singleNode* next;
+} t_singleNode;
 
-typedef struct node2 {
+typedef struct s_doubleNode {
 	int data;
 	struct node* prev;
-	struct node* next;
-} doubleNode;
-
-
+	struct t_doubleNode* next;
+} t_doubleNode;
 
 #endif
